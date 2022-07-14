a5dd148dafd578104925cb69681c3d0bc56dc923
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-07-15 01:10:43 +0400

    wo

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -647,7 +647,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-    await query.answer('Mmmm.... എന്നിട്ട്...!!!', show_alert=True)
+    await query.answer('Working...', show_alert=True)
 
     
 async def auto_filter(client, msg, spoll=False):
